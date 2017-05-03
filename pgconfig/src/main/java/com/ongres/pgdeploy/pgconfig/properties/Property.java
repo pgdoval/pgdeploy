85ac8ac7125f62f97aaf487299f656599a75d0d9
Pablo González Doval <pablo@8kdata.com>
2017-05-03 15:55:58 +0200

    Added Property.parse javadoc

--- pgconfig/src/main/java/com/ongres/pgdeploy/pgconfig/properties/Property.java
+++ pgconfig/src/main/java/com/ongres/pgdeploy/pgconfig/properties/Property.java
@@ -70,6 +70,32 @@ public class Property {
     this.availableUnits = availableUnits;
   }
 
+  /** Parses a given object into a {@link PropertyValue} representing it.
+   * @param obj The object to be parsed. It cannot be null and has to be compliant to one of these
+   *            rules:
+   *            <ul>
+   *            <li>Be an integer, a long, a float or a double. Depending on the property's
+   *            <tt>type</tt>, this will be parsed correctly or throw a
+   *            {@link WrongTypePropertyException}
+   *            <li>Be a String in one of the following styles:
+   *            <ul>
+   *              <li> <tt>"12"</tt>: Depending on the property's <tt>type</tt>, this will
+   *              return a {@link PropertyValue} containing either 12L, 12D or "12".
+   *              <li> <tt>"12MB"</tt>: It will return a {@link PropertyValue} whose value is
+   *              12D, 12L or even "12", and whose {@link Unit} will be MB.
+   *            </ul>
+   *            <li>More examples of valid values are: 1, 1L, 1.2F, 1.2D, "1", "2.3", "value",
+   *            "1TB", "2.2TB" or " 2 TB"
+   *            </ul>
+   * @return An instance of {@link PropertyValue} representing the received object
+   * @throws WrongTypePropertyException When the type of the property is different from that of the
+   *     value. Example: trying to parse the value 1.3F with  a Property whose <tt>type</tt> is
+   *     <tt>INTEGER</tt>.
+   * @throws UnitNotAvailableForPropertyException The object specifies a unit that is not contained
+   *     in the property's <tt>availableUnits</tt>. This includes trying to parse a value without
+   *     a unit from a property whose <tt>availableUnits</tt> don't include
+   *     {@link Unit}<tt>.NONE</tt>
+   */
   public PropertyValue parse(@Nonnull Object obj)
       throws WrongTypePropertyException, UnitNotAvailableForPropertyException {
 
